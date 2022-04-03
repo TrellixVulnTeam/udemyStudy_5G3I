5fe711396b4fa2c966b9818f9414bb1f5a3b0bc7
Tania Kozhemyaka <tvkozhemyaka@gmail.com>
2022-04-03 23:00:42 +0300

    q

--- script.js
+++ script.js
@@ -8,7 +8,25 @@ const options = {
         bg: "red"
     }
 };
+// это что бы перебрать Обьект в обьекте
+// for (let key in options) {
+//     if (typeof (options[key]) === 'object')
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} : ${options[key][i]}`);
+//         } else {
 
-for (let key in options) {
-    console.log(`Свойство ${key},`);
+//             console.log(`Свойство ${key} : ${options[key]}`);
+//         }
+// }
+
+// const ob = Object.keys(options).length;
+// console.log(ob)
+function sortingNum(a, b) {
+    return a - b;
 }
+
+const sortNum = [2, 8, 1, 15, 3, 18, 4];
+console.log(sortNum);
+
+sortNum.sort(sortingNum);
+console.log(sortNum.sort(sortingNum));
